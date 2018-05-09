0b531c824bf97ee33232b5cee7a3ed15555f1105
mehmetsinan.sahin <msinansahin@gmail.com>
2018-05-09 13:53:15 +0300

    added constructors to dtos

--- src/main/java/com/stella/commons/dto/PaymentDto.java
+++ src/main/java/com/stella/commons/dto/PaymentDto.java
@@ -1,8 +1,10 @@
 package com.stella.commons.dto;
 
 import com.stella.commons.Coin;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -11,6 +13,8 @@ import java.util.Set;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentDto implements Serializable {
 
     private Coin coin;
--- src/main/java/com/stella/commons/dto/PaymentInputDto.java
+++ src/main/java/com/stella/commons/dto/PaymentInputDto.java
@@ -1,13 +1,17 @@
 package com.stella.commons.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentInputDto implements Serializable {
 
     private String fromAddress;
